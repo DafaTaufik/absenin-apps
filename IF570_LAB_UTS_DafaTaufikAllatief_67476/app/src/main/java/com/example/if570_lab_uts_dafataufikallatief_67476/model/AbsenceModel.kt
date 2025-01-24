030ffdc2231b15faf2c72ea0a4a23c04345b9ccf
DafaTaufik <dafa.taufik@student.umn.ac.id>
2025-01-24 17:29:19 +0700

    update

--- IF570_LAB_UTS_DafaTaufikAllatief_67476/app/src/main/java/com/example/if570_lab_uts_dafataufikallatief_67476/model/AbsenceModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.if570_lab_uts_dafataufikallatief_67476.model
-
-data class Absence(
-    val imageUrl: String,
-    val date: String,
-    val time: String,
-    val status: String
-)
